9daf554f67b6e4848655b55133bcbdfb66d036d0
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-02-11 01:39:50 +0500

    Add product images, update product details, and enhance styling

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const mostBoughtProducts = [
-    {id: 1, name: "Mango", price: 350, quantity: 5, image: "https://picsum.photos/100/100"},
-    {id: 2, name: "Apples", price: 280, quantity: 10, image: "https://picsum.photos/100/100"},
-    {id: 3, name: "Potato", price: 150, quantity: 15, image: "https://picsum.photos/100/100"},
-    {id: 4, name: "Bread", price: 200, quantity: 8, image: "https://picsum.photos/100/100"},
-    {id: 5, name: "Milk", price: 120, quantity: 20, image: "https://picsum.photos/100/100"},
-    {id: 6, name: "Eggs", price: 180, quantity: 12, image: "https://picsum.photos/100/100"},
+    {id: 1, name: "Mango", price: 350, quantity: "500gm", image: "assets/mango.png"},
+    {id: 2, name: "Apples", price: 280, quantity: "500gm", image: "assets/apple.png"},
+    {id: 3, name: "Potato", price: 150, quantity: "1 kg", image: "assets/potato.png"},
+    {id: 4, name: "Bread", price: 200, quantity: "Medium", image: "assets/bread.png"},
+    {id: 5, name: "Milk", price: 120, quantity: "500 ML", image: "assets/milk.png"},
+    {id: 6, name: "Eggs", price: 180, quantity: "6", image: "assets/egg.png"},
 ]
 
 const featuredCategories = [
@@ -17,12 +17,14 @@ const featuredCategories = [
 
 function createProductCard(product) {
     const productCard = document.createElement('div');
-    productCard.className = 'w-60 h-50 rounded-xl flex flex-col gap-1 items-left p-2 hover:shadow-lg transition duration-300 cursor-pointer';
+    productCard.className = 'w-50 h-48 rounded-2xl flex flex-col bg-yellow-100 gap-1 items-left p-2 hover:shadow-lg transition duration-300 cursor-pointer text-center';
     productCard.innerHTML = `
-        <img src="${product.image}" alt="${product.name}" class="w-full h-32 object-cover rounded-tl-xl rounded-tr-xl">
+        <div class="w-full h-24 bg-white rounded-2xl flex items-center justify-center">
+            <img src="${product.image}" alt="${product.name}" class="h-24 object-contain">
+        </div>
         <span class="text-md font-medium">${product.name}</span>
-        <p class="text-sm">Price: ${product.price}</p>
-        <p class="text-sm">Quantity: ${product.quantity}</p>
+        <span class="text-xs">Fresh ${product.name} | ${product.quantity}</span>
+        <span class="text-sm font-medium mt-2">Rs. ${product.price}</span>
     `;
     return productCard;
 }
@@ -46,7 +48,7 @@ function createCategoryCard(category) {
     categoryCard.className = "w-35 md:w-42 h-22 bg-yellow-100 rounded-xl";
     categoryCard.innerHTML = `
     <span class="p-2 text-md font-medium">${category.name}</span>
-    <img src="${category.image}" alt="${category.name}" class="w-full h-16 object-cover rounded-tl-xl rounded-tr-xl">
+    <img src="${category.image}" alt="${category.name}" class="w-full h-16 object-contain rounded-tl-xl rounded-tr-xl">
     `;
     return categoryCard;
 }
