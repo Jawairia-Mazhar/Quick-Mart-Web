3bd3ebc7a81c611a1c0404ff3916e409b1cc734e
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-03-03 11:05:09 +0500

    Enhance cart functionality by adding error logging for missing products and added comments for better understanding

--- JS/cart.js
+++ JS/cart.js
@@ -2,6 +2,7 @@ let cart = {};
 let cartItems = null;
 
 const cartBtn = document.getElementById('cart-btn');
+
 cartBtn.addEventListener('click', () => {
     cartItems = document.createElement('div');
     cartItems.className = 'fixed top-0 z-70 right-0 bg-white w-64 h-full overflow-y-auto'
@@ -14,36 +15,38 @@ cartBtn.addEventListener('click', () => {
     <div id="cart-total" class="p-4 text-xl font-medium absolute bottom-0">   </div>
     `
     document.body.appendChild(cartItems);
-    if (cartItems) return;
+    if (cartItems) return; //This checks if cart has a value, function immediately exits then.
 
     renderCart();
 })
 
 function addToCart(productId){
     const product = mostBoughtProducts.find(p => p.id === Number(productId));
-    if (!product) {
+
+    if (!product) {    // If no product is found, log an error and stop the function
     console.error('Product not found:', productId);
     return;
   }
 
-    if (!cart[product.id]) {
-        cart[product.id] = { ...product, qty: 1 };
+    if (!cart[product.id]) { //if the product does not exist in the cart, add it in the cart
+        cart[product.id] = { ...product, qty: 1 }; //spread the product details and add initial quantity of 1
     } else {
-        cart[product.id].qty += 1;
+        cart[product.id].qty += 1; // if the product exists already in the cart, then increase the quantity by 1
     }
 
     const close = cartItems.querySelector('#close-cart');
     close.addEventListener('click', () => {
-        cartItems.remove();
-        cartItems = null;
+        cartItems.remove(); //updates the UI by removing the cart items.
+        cartItems = null; //resets the memory to null.
         document.body.style.overflow = ''; //restore default scrolling
     });
     renderCart(); 
 }
+
 function removeFromCart(productId) {
     if (cart[productId]) {
         delete(cart[productId]);
-        
+        console.log(`Product ${productId} removed from cart.`);
         renderCart();
     }
 }
@@ -72,12 +75,11 @@ function renderCart() {
     cartContainer.innerHTML = '';
     let total = 0;
 
-  Object.values(cart).forEach(item => {
+  Object.values(cart).forEach(item => { //Returns an array of cart object values, then iterate over each
     const price = Number(item.price) || 0;
     const qty = Number(item.qty) || 0;
     const lineTotal = price * qty;
-
-    total += lineTotal;
+    total += lineTotal; //Each items total is added to overall total price of the cart.
 
     cartContainer.innerHTML += `
       <div class="flex justify-between mb-2 items-center">
@@ -88,8 +90,7 @@ function renderCart() {
             class="w-4 h-4 cursor-pointer remove-item" 
             data-id="${item.id}">
       </div>
-    `;
-    
+    `; 
 });
     cartTotal.innerHTML = `<span class="text-sm">Total: Rs ${total.toFixed(2)}</span>`;
 }
